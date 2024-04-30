a9f41ca3cddd21e405b7fb27aac1a5f84913abf8
dek.haji1@gmail.com <dek.haji1@gmail.com>
2024-04-29 22:28:35 -0500

    added few more components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet,
+    HomeComponent,
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
 })
 export class AppComponent {
-  title = 'RealStateApp';
+  title = 'Homes';
 }
---
+++ src/app/housinglocation.ts
@@ -0,0 +1,11 @@
+export interface Housinglocation {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    photo: string;
+    availability: number;
+    wifi: boolean;
+    parking: boolean;
+    laundry: boolean;
+}
